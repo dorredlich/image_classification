235fd6bd342b485fa630723c0b65f3305c8283dd
dorredlich <Daviddegea1!>
2021-01-03 14:20:46 +0200

    changing

--- CNN.py
+++ CNN.py
@@ -1,83 +1,140 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
-import os
-import numpy as np
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import SGD
+
+
 import matplotlib.pyplot as plt
 
-batch_size = 128
-IMG_HEIGHT = 256
-IMG_WIDTH = 256
+
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same',
+                     input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def plotAccuracyLossForValidationAndTrain(model_fit_history):
+    acc = model_fit_history.history['acc']
+    val_acc = model_fit_history.history['val_acc']
+
+    loss = model_fit_history.history['loss']
+    val_loss = model_fit_history.history['val_loss']
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+
+def plotAccuracyLossForTestAndTrain(model_fit_history):
+    acc = model_fit_history.history['acc']
+    val_acc = model_fit_history.history['val_acc']
+
+    loss = model_fit_history.history['loss']
+    val_loss = model_fit_history.history['val_loss']
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Test Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Test Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Test Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Test Loss')
+    plt.show()
+
+
+epochs = 50
+batch_size = 32
+IMG_SIZE = 150
+
 
 train_path = 'dogs vs cats/training_set/'
 validation_path = 'dogs vs cats/validation_set/'
-test_path = 'dogs vs cats/test_set'
-
-# train = ImageDataGenerator(rescale=1. / 255,
-#                            width_shift_range=0.1,
-#                            height_shift_range=0.1,
-#                            horizontal_flip=True)
-datagen = ImageDataGenerator(rescale=1. / 255)
-
-train_dataset = datagen.flow_from_directory(train_path,
-                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                            batch_size=batch_size,
-                                            class_mode='binary')
-
-validation_dataset = datagen.flow_from_directory(validation_path,
-                                                 target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                 batch_size=batch_size,
-                                                 class_mode='binary')
-
-test_dataset = datagen.flow_from_directory(test_path,
-                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                           batch_size=batch_size,
-                                           class_mode='binary')
-
-model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu',
-           input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
-    MaxPooling2D(),
-    Dropout(0.2),
-    Conv2D(32, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Conv2D(64, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Dropout(0.2),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dense(1, activation='sigmoid')])
-
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-model_fit = model.fit_generator(train_dataset,
-                                steps_per_epoch=20,
-                                epochs=11,
-                                validation_data=test_dataset,
-                                validation_steps=len(test_dataset))
+test_path = 'dogs vs cats/test_set/'
 
 
-acc = model_fit.history['acc']
-val_acc = model_fit.history['val_acc']
+def run_test_harness():
 
-loss = model_fit.history['loss']
-val_loss = model_fit.history['val_loss']
+    model = define_model()
+    train_datagen = ImageDataGenerator(rotation_range=15,
+                                       rescale=1./255,
+                                       shear_range=0.1,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True,
+                                       width_shift_range=0.1,
+                                       height_shift_range=0.1)
 
-epochs_range = range(10)
+    test_datagen = ImageDataGenerator(rescale=1.0/255.0)
+    validation_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+    # prepare iterators
+    train_dataset = train_datagen.flow_from_directory(train_path,
+                                                      class_mode='binary',
+                                                      batch_size=batch_size,
+                                                      shuffle=True,
+                                                      target_size=(IMG_SIZE, IMG_SIZE))
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Test Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Test Accuracy')
+    validation_dataset = validation_datagen.flow_from_directory(validation_path,
+                                                                class_mode='binary',
+                                                                batch_size=batch_size,
+                                                                target_size=(IMG_SIZE, IMG_SIZE))
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Test Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Test Loss')
-plt.show()
+    test_dataset = test_datagen.flow_from_directory(test_path,
+                                                    class_mode='binary',
+                                                    batch_size=batch_size,
+                                                    shuffle=True,
+                                                    target_size=(IMG_SIZE, IMG_SIZE))
+
+    monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
+    # fit model
+
+    history = model.fit_generator(train_dataset,
+                                  steps_per_epoch=len(train_dataset),
+                                  validation_data=test_dataset,
+                                  validation_steps=len(test_dataset),
+                                  epochs=epochs,
+                                  callbacks=[monitor])
+
+
+    # evaluate model
+    _, acc = model.evaluate_generator(test_dataset, steps=len(test_dataset))
+    print('> %.3f' % (acc * 100.0))
+    # learning curves
+    plotAccuracyLossForTestAndTrain(history)
+
+ #   plotAccuracyLossForValidationAndTrain(history)
+
+
+run_test_harness()
+
+
+
